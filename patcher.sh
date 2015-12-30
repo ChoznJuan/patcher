180ed88047a6f8206266cf5a2dec88c81e41a2a5
D. Juan Gray <djg5826@gmail.com>
2015-12-30 17:25:36 -0500

    removed unneeded commits for my frameworks

--- patcher.sh
+++ patcher.sh
@@ -2,18 +2,9 @@
 
 # Apply these patches before compilation:
 
-# frameworks/av:
-cd frameworks/av
-git reset --hard
-wget https://github.com/CyanogenMod/android_frameworks_av/commit/df31a3368272dcf5671a1ca569d4769c205debad.patch
-patch -p1 < df31a3368272dcf5671a1ca569d4769c205debad.patch
-git clean -f -d
-
 # frameworks/base:
-cd ../../frameworks/base
+cd ~/rr/frameworks/base
 git reset --hard
-wget https://github.com/CyanogenMod/android_frameworks_base/commit/b68ae542f9999a6314e3bc6a13db800e18d14fe5.patch
-patch -p1 < b68ae542f9999a6314e3bc6a13db800e18d14fe5.patch
 wget https://github.com/CyanogenMod/android_frameworks_base/commit/e75f59e7fd349dd1fa5d452086c795f693776d89.patch
 patch -p1 < e75f59e7fd349dd1fa5d452086c795f693776d89.patch
 wget https://github.com/sultanxda/android_frameworks_base/commit/0cbd4a88767d78640b7dd391674575f7d5e517e6.patch
